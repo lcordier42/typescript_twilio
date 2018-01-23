a5da4d566cccf4e6202e1e29a1af13a48aa78cce
Brice BERNARD <brikou@gmail.com>
2018-01-23 18:33:25 +0100

    Add jest deps

--- dev.sh
+++ dev.sh
@@ -6,10 +6,12 @@ tmux new-session -d
 
 tmux split-window -t :0.0 -v
 tmux split-window -t :0.0 -v
+tmux split-window -t :0.0 -v
 
 tmux select-layout tiled
 
 tmux send-keys -t :0.0 'node_modules/.bin/tsc --pretty --watch' C-m
-tmux send-keys -t :0.1 'node_modules/.bin/nodemon --watch server.js server.js' C-m
+tmux send-keys -t :0.1 'node_modules/.bin/jest --watch' C-m
+tmux send-keys -t :0.2 'node_modules/.bin/nodemon --watch server.js server.js' C-m
 
 tmux attach-session
