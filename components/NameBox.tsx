c84d8166ca19ec3245e85278c905d55fa35ca56b
Brice BERNARD <brikou@gmail.com>
2018-01-23 06:44:19 +0100

    Simplify NameBox component

--- components/NameBox.tsx
+++ components/NameBox.tsx
@@ -1,35 +1,19 @@
 import * as React from "react";
 
-type NameBoxProps = {
+export const NameBox: React.SFC<{
     name: string;
     onNameChanged: any;
     logIn: any;
-};
-
-export class NameBox extends React.Component<NameBoxProps> {
-    render() {
-        const name = this.props.name;
-        const onNameChanged = this.props.onNameChanged;
-        const logIn = this.props.logIn;
-        return (
-            <div>
-                <form onSubmit={logIn}>
-                <select
-                        name="name"
-                        id="name"
-                        onChange={onNameChanged}
-                        value={name}
-                    >
-                        <option value="guest">guest</option>
-                        <option value="business">business</option>
-                        <option value="candidat">candidat</option>
-                        <option value="coach">coach</option>
-                    </select>
-                    <button type="submit">Log in</button>
-                </form>
-            </div>
-        );
-    }
-}
-
-export default NameBox;
+}> = ({ name, onNameChanged, logIn }) => (
+    <div>
+        <form onSubmit={logIn}>
+            <select name="name" id="name" onChange={onNameChanged} value={name}>
+                <option value="guest">guest</option>
+                <option value="business">business</option>
+                <option value="candidat">candidat</option>
+                <option value="coach">coach</option>
+            </select>
+            <button type="submit">Log in</button>
+        </form>
+    </div>
+);
