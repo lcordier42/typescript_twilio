4a1f1a53bdda1736b6628aa5ddc74bc9f2faaed0
Brice BERNARD <brikou@gmail.com>
2018-01-23 18:52:56 +0100

    Add puppeter deps

--- __tests__/sample.test.ts
+++ __tests__/sample.test.ts
@@ -1,3 +1,44 @@
-test("sample", () => {
+import * as dotenv from "dotenv";
+import * as puppeteer from "puppeteer";
+
+dotenv.config();
+
+let browserCandidate: puppeteer.Browser;
+let pageCandidate: puppeteer.Page;
+
+let browserCoach: puppeteer.Browser;
+let pageCoach: puppeteer.Page;
+
+let browserEmployer: puppeteer.Browser;
+let pageEmployer: puppeteer.Page;
+
+beforeAll(async () => {
+    const launchOptions: puppeteer.LaunchOptions = { headless: false };
+
+    browserCandidate = await puppeteer.launch(launchOptions);
+    pageCandidate = await browserCandidate.newPage();
+
+    browserCoach = await puppeteer.launch(launchOptions);
+    pageCoach = await browserCandidate.newPage();
+
+    browserEmployer = await puppeteer.launch(launchOptions);
+    pageEmployer = await browserEmployer.newPage();
+});
+
+test("sample", async () => {
+    await pageCandidate.goto(`http://localhost:${process.env.PORT}`);
+
+    await pageCoach.goto(`http://localhost:${process.env.PORT}`);
+
+    await pageEmployer.goto(`http://localhost:${process.env.PORT}`);
+
     expect(true).toBe(true);
+});
+
+afterAll(async () => {
+    await browserCandidate.close();
+
+    await browserCoach.close();
+
+    await browserEmployer.close();
 });
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { ChatApp } from "../components/ChatApp";
 export default () => (
     <div>
         <header>
-            <h1>Test Nexten Chat</h1>
+            <h1>Test Chat</h1>
         </header>
         <ChatApp />
     </div>
